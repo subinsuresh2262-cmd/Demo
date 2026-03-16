759aa2c1f7c4937f19eb5893a7fc5038b80f0991
subin <subin.sresh2262@gmail.com>
2026-03-16 15:40:26 +0400

    last one

--- demo/demo/report/outstanding_statement/outstanding_statement.py
+++ demo/demo/report/outstanding_statement/outstanding_statement.py
@@ -1,73 +1,25 @@
 import json
-
 import frappe
 from frappe import _
 from frappe.utils import flt, getdate, nowdate
 
-
 def execute(filters=None):
     filters = filters or {}
-
     columns = get_columns()
     data = get_data(filters)
-
     return columns, data
-
 
 def get_columns():
     return [
-        {
-            "label": _("Posting Date"),
-            "fieldname": "posting_date",
-            "fieldtype": "Date",
-            "width": 110,
-        },
-        {
-            "label": _("Ref"),
-            "fieldname": "voucher_no",
-            "fieldtype": "Dynamic Link",
-            "options": "voucher_type",
-            "width": 180,
-        },
-        {
-            "label": _("Description"),
-            "fieldname": "description",
-            "fieldtype": "Data",
-            "width": 260,
-        },
-        {
-            "label": _("LPO Number"),
-            "fieldname": "po_no",
-            "fieldtype": "Data",
-            "width": 140,
-        },
-        {
-            "label": _("Debit"),
-            "fieldname": "debit",
-            "fieldtype": "Currency",
-            "width": 120,
-        },
-        {
-            "label": _("Credit"),
-            "fieldname": "credit",
-            "fieldtype": "Currency",
-            "width": 120,
-        },
-        {
-            "label": _("Balance"),
-            "fieldname": "balance",
-            "fieldtype": "Currency",
-            "width": 120,
-        },
-        {
-            "label": _("Voucher Type"),
-            "fieldname": "voucher_type",
-            "fieldtype": "Data",
-            "hidden": 1,
-            "width": 120,
-        },
+        {"label": _("Posting Date"), "fieldname": "posting_date", "fieldtype": "Date", "width": 110},
+        {"label": _("Ref"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 180},
+        {"label": _("Description"), "fieldname": "description", "fieldtype": "Data", "width": 260},
+        {"label": _("LPO Number"), "fieldname": "po_no", "fieldtype": "Data", "width": 140},
+        {"label": _("Debit"), "fieldname": "debit", "fieldtype": "Currency", "width": 120},
+        {"label": _("Credit"), "fieldname": "credit", "fieldtype": "Currency", "width": 120},
+        {"label": _("Balance"), "fieldname": "balance", "fieldtype": "Currency", "width": 120},
+        {"label": _("Voucher Type"), "fieldname": "voucher_type", "fieldtype": "Data", "hidden": 1, "width": 120},
     ]
-
 
 def get_data(filters):
     conditions = [
@@ -94,55 +46,81 @@ def get_data(filters):
 
     where_clause = " AND ".join(conditions)
 
+    # SQL query to get GL Entries with proper outstanding logic
     rows = frappe.db.sql(
-    f"""
-    SELECT
-        gle.posting_date,
-        gle.voucher_type,
-        gle.voucher_no,
-        gle.party,
-        COALESCE(gle.remarks, '') AS description,
-        CASE
-            WHEN gle.voucher_type = 'Sales Invoice' THEN COALESCE(si.po_no, '')
-            ELSE ''
-        END AS po_no,
-        gle.debit_in_account_currency AS debit,
-        gle.credit_in_account_currency AS credit
-    FROM `tabGL Entry` gle
-    LEFT JOIN `tabSales Invoice` si
-        ON si.name = gle.voucher_no
-    WHERE {where_clause}
-    AND gle.voucher_type = 'Sales Invoice'
-    AND IFNULL(si.outstanding_amount,0) > 0
-    ORDER BY gle.posting_date ASC, gle.creation ASC, gle.name ASC
-    """,
-    values,
-    as_dict=1,
-)
+        f"""
+        SELECT
+            gle.posting_date,
+            gle.voucher_type,
+            gle.voucher_no,
+            gle.party,
+            COALESCE(gle.remarks, '') AS description,
+            CASE
+                WHEN gle.voucher_type = 'Sales Invoice' THEN COALESCE(si.po_no, '')
+                ELSE ''
+            END AS po_no,
+            CASE
+                WHEN gle.voucher_type = 'Sales Invoice' THEN si.outstanding_amount
+                ELSE gle.debit_in_account_currency
+            END AS debit,
+            CASE
+                WHEN gle.voucher_type = 'Sales Invoice' THEN 0
+                WHEN gle.voucher_type = 'Payment Entry' THEN
+                    gle.credit_in_account_currency - IFNULL((
+                        SELECT SUM(ref.allocated_amount)
+                        FROM `tabPayment Entry Reference` ref
+                        WHERE ref.parent = gle.voucher_no
+                          AND ref.parenttype = 'Payment Entry'
+                          AND ref.docstatus = 1
+                    ), 0)
+                ELSE gle.credit_in_account_currency
+            END AS credit
+        FROM `tabGL Entry` gle
+        LEFT JOIN `tabSales Invoice` si
+            ON si.name = gle.voucher_no
+            AND si.docstatus = 1
+        WHERE {where_clause}
+        AND (
+            (gle.voucher_type = 'Sales Invoice' AND COALESCE(si.outstanding_amount, 0) > 0)
+            OR (gle.voucher_type != 'Sales Invoice' AND 
+                (gle.voucher_type != 'Payment Entry' OR 
+                 gle.against_voucher IS NULL OR 
+                 gle.credit_in_account_currency - IFNULL((
+                     SELECT SUM(ref.allocated_amount)
+                     FROM `tabPayment Entry Reference` ref
+                     WHERE ref.parent = gle.voucher_no
+                       AND ref.parenttype = 'Payment Entry'
+                       AND ref.docstatus = 1
+                 ), 0) > 0
+                )
+            )
+        )
+        ORDER BY gle.posting_date ASC, gle.creation ASC, gle.name ASC
+        """,
+        values,
+        as_dict=1,
+    )
+
     balance = 0
     data = []
-
     for row in rows:
         debit = flt(row.debit)
         credit = flt(row.credit)
         balance += debit - credit
 
-        data.append(
-            {
-                "posting_date": row.posting_date,
-                "voucher_type": row.voucher_type,
-                "voucher_no": row.voucher_no,
-                "party": row.party,
-                "description": row.description or "",
-                "po_no": row.po_no or "",
-                "debit": debit,
-                "credit": credit,
-                "balance": balance,
-            }
-        )
+        data.append({
+            "posting_date": row.posting_date,
+            "voucher_type": row.voucher_type,
+            "voucher_no": row.voucher_no,
+            "party": row.party,
+            "description": row.description or "",
+            "po_no": row.po_no or "",
+            "debit": debit,
+            "credit": credit,
+            "balance": balance,
+        })
 
     return data
-
 
 @frappe.whitelist()
 def get_print_data(filters=None):
@@ -150,7 +128,6 @@ def get_print_data(filters=None):
         filters = json.loads(filters)
 
     filters = filters or {}
-
     data = get_data(filters)
 
     company = filters.get("company")
@@ -165,14 +142,11 @@ def get_print_data(filters=None):
 
     credit_limit = 0
     if customer and company:
-        credit_limit = (
-            frappe.db.get_value(
-                "Customer Credit Limit",
-                {"parent": customer, "company": company},
-                "credit_limit",
-            )
-            or 0
-        )
+        credit_limit = frappe.db.get_value(
+            "Customer Credit Limit",
+            {"parent": customer, "company": company},
+            "credit_limit",
+        ) or 0
 
     current_balance = flt(data[-1]["balance"]) if data else 0
     pdc_in_hand = 0
@@ -208,7 +182,6 @@ def get_print_data(filters=None):
         "rows": grouped_rows,
     }
 
-
 def build_grouped_rows(data):
     grouped_rows = []
     current_month = None
@@ -221,23 +194,18 @@ def build_grouped_rows(data):
 
         if current_month != month_label:
             if current_month is not None:
-                grouped_rows.append(
-                    {
-                        "row_type": "month_total",
-                        "description": "Total",
-                        "debit": flt(month_debit),
-                        "credit": flt(month_credit),
-                        "balance": flt(previous_balance),
-                    }
-                )
+                grouped_rows.append({
+                    "row_type": "month_total",
+                    "description": "Total",
+                    "debit": flt(month_debit),
+                    "credit": flt(month_credit),
+                    "balance": flt(previous_balance),
+                })
 
-            grouped_rows.append(
-                {
-                    "row_type": "month_header",
-                    "description": month_label,
-                }
-            )
-
+            grouped_rows.append({
+                "row_type": "month_header",
+                "description": month_label,
+            })
             current_month = month_label
             month_debit = 0
             month_credit = 0
@@ -246,47 +214,38 @@ def build_grouped_rows(data):
         month_credit += flt(row.get("credit"))
         previous_balance = flt(row.get("balance"))
 
-        grouped_rows.append(
-            {
-                "row_type": "data",
-                "posting_date": row.get("posting_date"),
-                "voucher_no": row.get("voucher_no"),
-                "description": row.get("description"),
-                "po_no": row.get("po_no"),
-                "debit": flt(row.get("debit")),
-                "credit": flt(row.get("credit")),
-                "balance": flt(row.get("balance")),
-            }
-        )
+        grouped_rows.append({
+            "row_type": "data",
+            "posting_date": row.get("posting_date"),
+            "voucher_no": row.get("voucher_no"),
+            "description": row.get("description"),
+            "po_no": row.get("po_no"),
+            "debit": flt(row.get("debit")),
+            "credit": flt(row.get("credit")),
+            "balance": flt(row.get("balance")),
+        })
 
     if current_month is not None:
-        grouped_rows.append(
-            {
-                "row_type": "month_total",
-                "description": "Total",
-                "debit": flt(month_debit),
-                "credit": flt(month_credit),
-                "balance": flt(previous_balance),
-            }
-        )
+        grouped_rows.append({
+            "row_type": "month_total",
+            "description": "Total",
+            "debit": flt(month_debit),
+            "credit": flt(month_credit),
+            "balance": flt(previous_balance),
+        })
 
     return grouped_rows
-
 
 def get_letter_head_html(company):
     if not company:
         return ""
 
     try:
-        # First preference: company default letter head
         letter_head_name = frappe.db.get_value("Company", company, "default_letter_head")
 
-        # Fallback: any enabled default letter head
         if not letter_head_name:
             letter_head_name = frappe.db.get_value(
-                "Letter Head",
-                {"is_default": 1, "disabled": 0},
-                "name"
+                "Letter Head", {"is_default": 1, "disabled": 0}, "name"
             )
 
         if not letter_head_name:
@@ -294,11 +253,9 @@ def get_letter_head_html(company):
 
         lh = frappe.get_doc("Letter Head", letter_head_name)
 
-        # HTML based letter head
         if lh.get("content"):
             return frappe.render_template(lh.content, {"doc": lh})
 
-        # Image based letter head
         if lh.get("image"):
             align = (lh.get("align") or "Left").lower()
             return f"""
@@ -306,18 +263,14 @@ def get_letter_head_html(company):
                     <img src="{lh.image}" style="max-width:100%; max-height:140px;">
                 </div>
             """
-
         return ""
-
     except Exception:
         frappe.log_error(frappe.get_traceback(), "Outstanding Statement Letter Head Error")
         return ""
 
-
 def get_customer_primary_address(customer_doc):
     if not customer_doc:
         return ""
-
     address_name = getattr(customer_doc, "customer_primary_address", None)
     if not address_name:
         return ""
@@ -325,14 +278,7 @@ def get_customer_primary_address(customer_doc):
     address = frappe.db.get_value(
         "Address",
         address_name,
-        [
-            "address_line1",
-            "address_line2",
-            "city",
-            "state",
-            "country",
-            "pincode",
-        ],
+        ["address_line1","address_line2","city","state","country","pincode"],
         as_dict=1,
     )
 
@@ -347,5 +293,4 @@ def get_customer_primary_address(customer_doc):
         address.get("country"),
         address.get("pincode"),
     ]
-
     return ", ".join([p for p in parts if p])
